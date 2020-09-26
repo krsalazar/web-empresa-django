89022115d50d4dafbd6d391bf9a1a54fd993fced
Kevin Raí Salazar <71470078+krsalazar@users.noreply.github.com>
2020-09-25 20:53:37 -0600

    Update settings.py

--- WebNegocio/WebNegocio/settings.py
+++ WebNegocio/WebNegocio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q(g63ohwc#i5ugrfmhl5*t%5e#(**-@=vhn%gy7%x)3s)2cnew'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
